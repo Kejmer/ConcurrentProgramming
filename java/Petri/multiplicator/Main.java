6cccd49f48c63a3344d56f54432c7be499f95a90
Kejmer <kajetanhus@gmail.com>
2019-11-15 10:53:06 +0100

    reachable method

--- java/Petri/multiplicator/Main.java
+++ java/Petri/multiplicator/Main.java
@@ -14,82 +14,72 @@ public class Main {
 		InputA, InputB, BufferA, BufferB, Result
 	}
 	
-	private static Map<Place, Integer> marking(int a, int b, int c, int d) {
-        Map<Place, Integer> result = new HashMap<>();
-        putPositive(result, Place.A, a);
-        putPositive(result, Place.B, b);
-        putPositive(result, Place.C, c);
-        putPositive(result, Place.D, d);
-        return result;
-    }
-	
 	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);  
+    int a = reader.nextInt();  
+		int b = reader.nextInt();
+		
+		//Building the net
+		//Places
+		Map<Place, Integer> begin = new HashMap<>();
+		begin.put(Place.InputA, a);
+		begin.put(Place.InputB, b);
+		PetriNet<Place, Integer> net = new PetriNet<>(begin, false);
+		
+		//Transitions
+		ArrayList<Transition<Place>> transitions = new ArrayList<>();
+		HashMap<Place, Integer> input = new HashMap<>(), output = new HashMap<>();
+		HashSet<Place> empty = new HashSet<>(), inhibitor = new HashSet<>();
+		
+		//InputA -> BufferA
+		input.put(Place.InputA, 1);
+		output.put(Place.BufferA, 1);
+		inhibitor.add(Place.BufferA);
+		inhibitor.add(Place.BufferB);
+		
+		transitions.add(new Transition<Place>(input.clone(), empty.clone(), inhibitor.clone(), output.clone()));
+		input.clear();
+		output.clear();
+		inhibitor.clear();
+		
+		//calculating result
+		input.put(Place.InputB, 1);
+		input.put(Place.BufferA, 1);
+		output.put(Place.Result, 1);
+		output.put(Place.BufferB, 1);
+		output.put(Place.BufferA, 1);
+		
+		transitions.add(new Transition<Place>(input.clone(), empty.clone(), inhibitor.clone(), output.clone()));
+		input.clear();
+		output.clear();
+		
+		//refiling B
+		input.put(Place.BufferB, 1);
+		output.put(Place.InputB, 1);
+		inhibitor.add(Place.BufferA);
+		
+		transitions.add(new Transition<Place>(input.clone(), empty.clone(), inhibitor.clone(), output.clone()));
+		input.clear();
+		output.clear();
+		inhibitor.clear();
+		
+		//Substracting 1 from multiplier
+		input.put(Place.BufferB, 1);
+		inhibitor.add(Place.InputA, 1);
+		
+		transitions.add(new Transition<Place>(input.clone(), empty.clone(), inhibitor.clone(), output.clone()));
+		input.clear();
+		inhibitor.clear();
+		
+		//Final transition
+		inhibitor.add(Place.InputA);
+		inhibitor.add(Place.BufferA);
+		
+		Transition<Place> finalTransition = new Transition<>(input.clone(), empty.clone(), inhibitor.clone(), output.clone());
+		inhibitor.clear();
+		
+		//Petri net is finished
 		try {
-			Scanner reader = new Scanner(System.in);  
-	    int a = reader.nextInt();  
-			int b = reader.nextInt();
-			
-			//Building the net
-			//Places
-			Map<Place, Integer> begin = new HashMap<>();
-			begin.put(Place.InputA, a);
-			begin.put(Place.InputB, b);
-			PetriNet<Place, Integer> net = new PetriNet<>(begin, false);
-			
-			//Transitions
-			ArrayList<Transition<Place>> transitions = new ArrayList<>();
-			HashMap<Place, Integer> input = new HashMap<>(), output = new HashMap<>();
-			HashSet<Place> empty = new HashSet<>(), inhibitor = new HashSet<>();
-			
-			//InputA -> BufferA
-			input.put(Place.InputA, 1);
-			output.put(Place.BufferA, 1);
-			inhibitor.add(Place.BufferA);
-			inhibitor.add(Place.BufferB);
-			
-			transitions.add(new Transition<Place>(input.clone(), empty.clone(), inhibitor.clone(), output.clone()));
-			input.clear();
-			output.clear();
-			inhibitor.clear();
-			
-			//calculating result
-			input.put(Place.InputB, 1);
-			input.put(Place.BufferA, 1);
-			output.put(Place.Result, 1);
-			output.put(Place.BufferB, 1);
-			output.put(Place.BufferA, 1);
-			
-			transitions.add(new Transition<Place>(input.clone(), empty.clone(), inhibitor.clone(), output.clone()));
-			input.clear();
-			output.clear();
-			
-			//refiling B
-			input.put(Place.BufferB, 1);
-			output.put(Place.InputB, 1);
-			inhibitor.add(Place.BufferA);
-			
-			transitions.add(new Transition<Place>(input.clone(), empty.clone(), inhibitor.clone(), output.clone()));
-			input.clear();
-			output.clear();
-			inhibitor.clear();
-			
-			//Substracting 1 from multiplier
-			input.put(Place.BufferB, 1);
-			inhibitor.add(Place.InputA, 1);
-			
-			transitions.add(new Transition<Place>(input.clone(), empty.clone(), inhibitor.clone(), output.clone()));
-			input.clear();
-			inhibitor.clear();
-			
-			//Final transition
-			
-			inhibitor.add(Place.InputA);
-			inhibitor.add(Place.BufferA);
-			
-			Transition<Place> finalTransition = new Transition<>(input.clone(), empty.clone(), inhibitor.clone(), output.clone());
-			inhibitor.clear();
-			
-			//Petri net is finished
 			
 
 		}
--- java/Petri/petrinet/PetriNet.java
+++ java/Petri/petrinet/PetriNet.java
@@ -4,11 +4,12 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 import java.util.Queue;
+import java.util.HashSet;
 
 public class PetriNet<T> {
 	
 	private class ReachableHelper<T> {
-		Set<Map<T, Integer>> possibleTokenization = new Set<>();
+		Set<Map<T, Integer>> possibleTokenization = new HashSet<>();
 	}
 	
 	private Map<T, Integer> tokenization;
@@ -19,10 +20,24 @@ public class PetriNet<T> {
 		this.tokenization = initial;
 		this.fair = fair;
 	}
+	
+	private void generateReachable(Collection<Transition<T>> transitions, Set<Map<T, Integer>> possibleTokenization, Map<T, Integer> currentState) {
+		for (Transition<T> t : transitions) {
+			if (allowedTransition(t, currentState)) {
+				Map<T, Integer> tokenizationCopy = currentState.clone();
+				getNextState(t, tokenizationCopy);
+				if (possibleTokenization.contains(tokenizationCopy)) {
+					possibleTokenization.add(tokenizationCopy);
+					generateReachable(transitions, possibleTokenization, currentState);
+				}
+			}
+		}
+	}
 
 	public Set<Map<T, Integer>> reachable(Collection<Transition<T>> transitions) {
-		Set<Map<T, Integer>> possibleTokenization = new Set<>();
-		
+		Set<Map<T, Integer>> possibleTokenization = new HashSet<>();
+		generateReachable(transitions, possibleTokenization, tokenization);
+		return possibleTokenization;
 	}
 
 	public Transition<T> fire(Collection<Transition<T>> transitions) throws InterruptedException {
@@ -37,33 +52,38 @@ public class PetriNet<T> {
 		return new IncompatibleTransition(s);
 	}
 	
-	public boolean allowedTransition(Transition<T> trans) throws IncompatibleTransition {
+	//Zakładamy że przejście jest dozwolone
+	private void getNextState(Transition<T> trans, Map<T, Integer> state) {
+		
+	}
+	
+	private boolean allowedTransition(Transition<T> trans, Map<T, Integer> currentTokenization) throws IncompatibleTransition {
 		for (Map.Entry<T, Integer> entry : trans.getInput().entrySet()) {
-			if (tokenization.containsKey(entry.getKey())) {
+			if (currentTokenization.containsKey(entry.getKey())) {
 				throw createException(entry.getKey(), "input");
 			}
-			if (tokenization.get(entry.getKey()) < entry.getValue()) {
+			if (currentTokenization.get(entry.getKey()) < entry.getValue()) {
 				return false;
 			}
 		}
 		
 		for (Map.Entry<T, Integer> entry : trans.getOutput().entrySet()) {
-			if (tokenization.containsKey(entry.getKey())) {
+			if (currentTokenization.containsKey(entry.getKey())) {
 				throw createException(entry.getKey(), "output");
 			}
 		}
 		
 		for (T t : trans.getReset()) {
-			if (tokenization.containsKey(t)) {
+			if (currentTokenization.containsKey(t)) {
 				throw createException(t, "reset");
 			}
 		}
 		
 		for (T t : trans.getInhibitor()) {
-			if (tokenization.containsKey(t)) {
+			if (currentTokenization.containsKey(t)) {
 				throw createException(t, "inhibitor");
 			}
-			if (tokenization.get(t) > 0) {
+			if (currentTokenization.get(t) > 0) {
 				return false;
 			}
 		}
