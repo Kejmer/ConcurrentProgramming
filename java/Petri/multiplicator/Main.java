b7e97b8379636260ab2b9129c3e83919c51313cf
Kejmer <kajetanhus@gmail.com>
2019-11-18 00:27:51 +0100

    Multiplicator working

--- java/Petri/multiplicator/Main.java
+++ java/Petri/multiplicator/Main.java
@@ -1,12 +1,16 @@
 package multiplicator;
 
-import petrinet.*;
 import java.util.HashMap;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
+
+import petrinet.PetriNet;
+import petrinet.Transition;
 
 public class Main {
 	
@@ -23,7 +27,7 @@ public class Main {
     public Counter(Collection<Transition<Place>> transitions, PetriNet net, int idx) {
       this.transitions = transitions;
       this.net = net;
-      this.idx = idx
+      this.idx = idx;
       this.numOfTransitions = 0;
     }
 
@@ -35,7 +39,7 @@ public class Main {
           numOfTransitions++;
         }
       } catch (InterruptedException e) {
-      	System.out.println("Wątek " + idx + " wykonał " + numOfTransitions + " przejść.")
+      	System.out.println("Wątek " + idx + " wykonał " + numOfTransitions + " przejść.");
       }
     }
 	}
@@ -50,7 +54,7 @@ public class Main {
 		Map<Place, Integer> begin = new HashMap<>();
 		begin.put(Place.InputA, a);
 		begin.put(Place.InputB, b);
-		PetriNet<Place, Integer> net = new PetriNet<>(begin, false);
+		PetriNet<Place> net = new PetriNet<>(begin, false);
 		
 		//Transitions
 		ArrayList<Transition<Place>> transitions = new ArrayList<>();
@@ -90,8 +94,8 @@ public class Main {
 		inhibitor.clear();
 		
 		//Substracting 1 from multiplier
-		input.put(Place.BufferB, 1);
-		inhibitor.add(Place.InputA, 1);
+		input.put(Place.BufferA, 1);
+		inhibitor.add(Place.InputB);
 		
     transitions.add(new Transition<Place>(input, empty, inhibitor, output));
 		input.clear();
@@ -106,6 +110,7 @@ public class Main {
 		inhibitor.clear();
 		
 		//Petri net is finished
+		System.out.println("ZACZYNAMY MNOŻONKO");
 		try {
 			ArrayList<Thread> workers = new ArrayList<>();
 			for (int i = 0; i < 4; i++) {
@@ -118,21 +123,22 @@ public class Main {
 			net.fire(Collections.singleton(finalTransition));
 			
 			for (Thread t : workers) {
-				t.interupt();
+				t.interrupt();
 			}
 			
 			//Checking if every transition is no longer legal (except finalTransition)
-			Set<Map<Place, Integer>> possibleTokenization = net.rechable(transitions); 
+			Set<Map<Place, Integer>> possibleTokenization = net.reachable(transitions); 
 			
 			if (possibleTokenization.size() > 1) {
-				System.err.println("Niektóre przejścia wciąż są dozwolone")
+				System.err.println("Niektóre przejścia wciąż są dozwolone");
 			} else {
 				for (Map<Place, Integer> tokenization : possibleTokenization) {
 					int result = tokenization.get(Place.Result);
 					System.out.println(result);
 				}
 			}
- 
+		} catch (InterruptedException e) {
+			System.out.println("Wątek main został przerwany!");
 		}
 		
 	}
