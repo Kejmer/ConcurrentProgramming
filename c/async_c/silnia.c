2e776f1b58b9a1ac2e15029f287b851f19b27e1e
Kejmer <kajetanhus@gmail.com>
2020-01-07 08:18:31 +0100

    silnia 3 thread

--- c/async_c/silnia.c
+++ c/async_c/silnia.c
@@ -6,7 +6,6 @@
 typedef struct factorial
 {
   int step;
-  // int limit;
   uint64_t result;
 } factorial_t;
 
@@ -15,9 +14,8 @@ static void *part_factorial(void *arg, size_t argsz __attribute__((unused)),
 {
   factorial_t *factorial = (factorial_t *) arg;
   factorial_t *new_factorial = malloc(sizeof(factorial_t));
-  // new_factorial->limit = factorial->limit;
-  new_factorial->step = factorial->step+1;
-  new_factorial->result = new_factorial->step * factorial->result;
+  new_factorial->step = factorial->step + 3;
+  new_factorial->result = factorial->step * factorial->result;
   return (void *)new_factorial;
 }
 
@@ -33,30 +31,54 @@ int main() {
   int n;
   scanf("%d", &n);
 
-  factorial_t *factorial = malloc(sizeof(factorial_t));
-  factorial->step = 0;
-  factorial->result = 1;
+  if (n < 3) {
+    int res = n == 0 ? 1 : n;
+    printf("%d\n", res);
+    return 0;
+  }
 
   future_t *future = malloc(n * sizeof(future_t));
-  async(&pool, &future[0], (callable_t){.function = &part_factorial,
-                                        .arg = factorial,
-                                        .argsz = sizeof(factorial_t)});
-int map(thread_pool_t *pool, future_t *future, future_t *from,
-        void *(*function)(void *, size_t, size_t *));
-  for (int i = 1; i < n; i++) {
-    if ((err = map(&pool, &future[i], &future[i-1], &part_factorial))) {
+  if (future == NULL) {
+    fprintf(stderr, "bad malloc\n");
+  }
+  factorial_t **factorial = malloc(sizeof(factorial_t *) * 3);
+  if (factorial == NULL) {
+    fprintf(stderr, "bad malloc\n");
+  }
+  for (int i = 0; i < 3; i++) {
+    factorial[i] = malloc(sizeof(factorial_t));
+    if (factorial[i] == NULL) {
+      fprintf(stderr, "bad malloc\n");
+    }
+    factorial[i]->step = i+1;
+    factorial[i]->result = 1;
+    async(&pool, &future[i], (callable_t){.function = &part_factorial,
+                                          .arg = factorial[i],
+                                          .argsz = sizeof(factorial_t)});
+  }
+
+  for (int i = 3; i < n; i++) {
+    if ((err = map(&pool, &future[i], &future[i-3], &part_factorial))) {
       fprintf(stderr, "%d error – map\n", err);
       exit(-1);
     }
   }
 
-  factorial_t *result = (void *)await(&future[n-1]);
-  printf("%ld\n", result->result);
+  uint64_t result = 1;
+  for (int i = 1; i <= 3; i++) {
+    factorial_t *fact = (void *)await(&future[n-i]);
+    result *= fact->result;
+    free(fact);
+  }
+
+  printf("%ld\n", result);
 
   thread_pool_destroy(&pool);
+  for (int i = 0; i < 3; i++) {
+    free(factorial[i]);
+  }
   free(factorial);
   free(future);
-  free(result);
 
   return 0;
 }
